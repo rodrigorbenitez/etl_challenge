962375ce776a9468c51a594dfc6ecf5843bba453
Rodrigo Benitez <62714022+rodrigorbenitez@users.noreply.github.com>
2021-11-30 15:36:46 +0100

    edit functions in db.py

--- src/etl/db.py
+++ src/etl/db.py
@@ -18,8 +18,9 @@ def create():
     model = [
          """
         CREATE TABLE IF NOT EXISTS users (
-            createdAt TIMESTAMP,
-            updatedAt TIMESTAMP,
+            id INTEGER PRIMARY KEY,
+            createdAt TEXT NOT NULL,
+            updatedAt TEXT NOT NULL,
             firstName TEXT NOT NULL,
             lastName TEXT NOT NULL,
             address TEXT NOT NULL,
@@ -28,14 +29,29 @@ def create():
             zipCode TEXT NOT NULL,
             email TEXT NOT NULL,
             birthDate TEXT NOT NULL,
-            profile TEXT NOT NULL,
-            subscription TEXT NOT NULL,
-            id INTEGER PRIMARY KEY
+            gender TEXT NOT NULL,
+            isSmoking TEXT NOT NULL,
+            profession TEXT NOT NULL,
+            income TEXT NOT NULL
+            
 
         )
         """,
         """
-        CREATE TABLE IF NOT EXISTS subscriptions_and_messages (
+        CREATE TABLE IF NOT EXISTS subscriptions (
+            createdAt TEXT NOT NULL,
+            startDate TEXT NOT NULL,
+            endDate TEXT NOT NULL,
+            status TEXT NOT NULL,
+            amount TEXT NOT NULL,
+            FOREIGN KEY (id)
+               REFERENCES users (id)
+                    ON DELETE CASCADE 
+                    ON UPDATE NO ACTION         
+        )
+        """,
+                """
+        CREATE TABLE IF NOT EXISTS messages (
             createdAt TIMESTAMP,
             message TEXT NOT NULL,
             FOREIGN KEY (receiverId),
@@ -50,6 +66,7 @@ def create():
 
         )
         """
+        
     ]
      # Database Connection
     db = get()
@@ -59,6 +76,7 @@ def create():
     for table in model: cursor.execute(table)
 
     utils.log('Database Schema created!')
+     
 
 # Method to Load tables
 
@@ -90,25 +108,48 @@ def load(table, schema, values):
 
     utils.log(f'Load {table}')
 
+# Dataframes generation from url
+
 def users():
-    df_users = pd.read_json('https://619ca0ea68ebaa001753c9b0.mockapi.io/evaluation/dataengineer/jr/v1/users')
+    url_users='https://619ca0ea68ebaa001753c9b0.mockapi.io/evaluation/dataengineer/jr/v1/users'
+    df_users = pd.read_json(url_users)
+    profile_dict=df_users['profile']
+    df_profile = pd.DataFrame([x for x in profile_dict])
+    df_users=pd.concat([df_users, df_profile], axis=1)
+    df_users[['id','createdAt','updatedAt','firstName','lastName','address','city','country','zipCode','email','birthDate','gender','isSmoking','profession','income']]
     return df_users
 
-def df_subscription_and_messages():
-    df_users = pd.read_json('https://619ca0ea68ebaa001753c9b0.mockapi.io/evaluation/dataengineer/jr/v1/messages')
-    return df_subscription_and_messages
+def subscriptions():
+    url_users='https://619ca0ea68ebaa001753c9b0.mockapi.io/evaluation/dataengineer/jr/v1/users'    
+    df_users = pd.read_json(url_users)
+    subscription_df=df_users[['id','subscription']]
+    subscription_df = subscription_df.explode('subscription')
+    df = pd.DataFrame(columns = ['createdAt', 'startDate','endDate','status','amount','id'])
+    for index, row in subscription_df.iterrows():
+        if row['subscription'] is not Nan:
+            dicts=row['subscription']
+            dicts['id']=row['id']
+            df = df.append(dicts, ignore_index=True, sort=False)
+        else:
+            df2 = {'createdAt': Nan, 'startDate': Nan, 'endDate':Nan, 'status': Nan, 'amount': Nan, 'id': row['id'], }
+            df = df.append(df2, ignore_index = True)
+    return df
 
+def messages():
+    url_messages='https://619ca0ea68ebaa001753c9b0.mockapi.io/evaluation/dataengineer/jr/v1/messages'    
+    df_messages=pd.read_json(url_messages)     
+    return df_messages
 
 # Method to load database after creation with dimensions
 def initialize():
     utils.log('Initializing Database')
     utils.log('Generating users dimensions')
     users=users()
-    subscription_and_messages=df_subscription_and_messages()
+    subscriptions=subscriptions()
+    messages=messages()
     
-    load('users', ['createdAt','updatedAt','firstName','lastName','address','city','country','zipCode','email','birthDate','profile','subscription','id'], users)
-    load('subscription_and_messages', ['createdAt','message','receiverId','id','senderId'], subscription_and_messages)
-
-    
+    load('users', ['id','createdAt','updatedAt','firstName','lastName','address','city','country','zipCode','email','birthDate','gender','isSmoking','profession','income'], users)
+    load('subscriptions', ['createdAt','startDate','endDate','status','amount','id'], subscriptions)
+    load('messages', ['createdAt','message','receiverId','id','senderId'], messages)    
     
     utils.log('Initialized Database!')
